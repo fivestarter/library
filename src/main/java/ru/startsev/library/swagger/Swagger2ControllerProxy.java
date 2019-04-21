f75810d744a2805733abac69d0598ae2ecb6d5ed
yury <fivestarter@gmail.com>
2019-04-21 11:33:03 +0300

    refactoring

--- src/main/java/ru/startsev/library/swagger/Swagger2ControllerProxy.java
+++ src/main/java/ru/startsev/library/swagger/Swagger2ControllerProxy.java
@@ -1,26 +1,19 @@
 package ru.startsev.library.swagger;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Strings;
-import io.swagger.models.Swagger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponents;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.annotations.ApiIgnore;
-import springfox.documentation.service.Documentation;
-import springfox.documentation.spring.web.PropertySourcedMapping;
 import springfox.documentation.spring.web.json.Json;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.web.Swagger2Controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
-import static springfox.documentation.swagger.common.HostNameProvider.componentsFrom;
 
 @Controller
 @ApiIgnore
